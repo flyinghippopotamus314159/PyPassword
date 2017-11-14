411413102ea7bd03a61329dc93410291dea1d4e1
flyinghippopotamus314159 <h1409637@trbvm.com>
2017-11-14 16:56:58 +0000

    cleaned num_check() & added num_verif()

--- PyPassword.py
+++ PyPassword.py
@@ -6,11 +6,16 @@ def num_check(password):
   divNum=passNum[3:]
   endThree=passNumTwo[:3]
   divNumber=int(divNum)
-  print(divNumber)
   endNumber=int(endThree)
-  print(endNumber)
   storePass=divNumber%endNumber
   storeString=str(storePass)
   for i in range(3-len(storeString)):
     storeString="0"+storeString
   return(storeString)
+def num_verif(checkDigits,password):
+  checkTo=num_check(password)
+  if checkTo==str(checkDigits):
+    verif=True
+  else:
+    verif=False
+  return(verif)
