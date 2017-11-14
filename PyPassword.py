d85eaba3c911987b80fbf3ef15363b3eca6e8966
flyinghippopotamus314159 <h1409637@trbvm.com>
2017-11-14 16:51:10 +0000

    made num_check() function

--- PyPassword.py
+++ PyPassword.py
@@ -1,1 +1,16 @@
-a
+def num_check(password):
+  passNum=""
+  for i in range(len(password)):
+    passNum=passNum+str(ord(password[i]))
+  passNumTwo=passNum
+  divNum=passNum[3:]
+  endThree=passNumTwo[:3]
+  divNumber=int(divNum)
+  print(divNumber)
+  endNumber=int(endThree)
+  print(endNumber)
+  storePass=divNumber%endNumber
+  storeString=str(storePass)
+  for i in range(3-len(storeString)):
+    storeString="0"+storeString
+  return(storeString)
