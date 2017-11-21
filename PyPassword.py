d9026cba6ee266ae93af4ee71d7af3f43115ac6b
flyinghippopotamus314159 <h1409637@trbvm.com>
2017-11-21 12:25:47 +0000

    added initialise function

--- PyPassword.py
+++ PyPassword.py
@@ -1,4 +1,4 @@
-def num_check(password):
+def num_check(password):                                                                              #creates a three-digit check number by converting the password to a number, finding the modulo in the base of the last three base ten digits of the rest of the number: there are 1000 possible numbers, but it will be the same for any password
   passNum=""
   for i in range(len(password)):
     passNum=passNum+str(ord(password[i]))
@@ -12,10 +12,26 @@ def num_check(password):
   for i in range(3-len(storeString)):
     storeString="0"+storeString
   return(storeString)
-def num_verif(checkDigits,password):
+def num_verif(checkDigits,password):                                                                  #verifies, with the check number from num_check(), the password inputted
   checkTo=num_check(password)
   if checkTo==str(checkDigits):
     verif=True
   else:
     verif=False
   return(verif)
+def initialise():                                                                                     #sets up the program when running for the first time
+  file=open("PyPassword.txt","w")
+  while True:
+    masterKey=input("Please set a master password:")
+    masterKeyCheck=input("Please repeat for security:")
+    if masterKey==masterKeyCheck:
+      break
+  numCheckDigit=num_check(masterKey)
+  masterKey=""
+  file.write(str(str(numCheckDigit)+"|username:encryptedpassword;username:encryptedpassword"))
+  file.close()
+def addPassword(site,username,password):                                                            #adds a new password
+  file=open("PyPassword.txt","r+")
+  masterKey=input("Please enter the master password:")
+  
+ 
