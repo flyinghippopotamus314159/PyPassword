dacf2017ad64f95e522d1d6e532b0f271bc336f7
flyinghippopotamus314159 <h1409637@trbvm.com>
2017-12-01 13:19:22 +0000

    continued gen_password function

--- PyPassword.py
+++ PyPassword.py
@@ -30,9 +30,11 @@ def initialise():                                                               
   masterKey=""
   file.write(str(str(numCheckDigit)+"|username:encryptedpassword;username:encryptedpassword"))
   file.close()
-def gen_password():
+def gen_password(length):
   import random
   character=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','1','2','3','4','5']
+  for i in range(length):
+    
 def add_password(site,username,password):                                                            #adds a new password
   file=open("PyPassword.txt","r+")
   timeDelays=[0,0,5,15,60,120,300,1200,12000,120000]
